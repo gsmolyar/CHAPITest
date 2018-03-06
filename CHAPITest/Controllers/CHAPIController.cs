34bc06729b4a9abff4a44f766dea7b5c267ccd0d
Gary Smolyar <gsmolyar@hotmail.com>
2018-03-06 11:59:54 -0600

    Initial - added basic controller

---
+++ CHAPITest/Controllers/CHAPIController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CHAPITest.Controllers
+{
+    public class CHAPIController : ApiController
+    {
+        // GET: api/CHAPI
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/CHAPI/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/CHAPI
+        public void Post([FromBody]string value)
+        {
+            Console.Write("Hello World");
+            Console.ReadLine();
+        }
+
+        // PUT: api/CHAPI/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/CHAPI/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
